1acd3f187b7b82f1ec0471fa3376014fc7bfb148
HoaNgomta <32055817+HoaNgomta@users.noreply.github.com>
2017-11-11 20:02:23 -0800

    ok

--- QLBanHang/QLBanHang/GUI/FrmMatHangKinhDoanh.cs
+++ QLBanHang/QLBanHang/GUI/FrmMatHangKinhDoanh.cs
@@ -8,13 +8,165 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QLBanHang.GUI
+namespace QuanLyHangHoa.GUI
 {
     public partial class FrmMatHangKinhDoanh : Form
     {
+        private QUANLYHANGHOAEntities db = new QUANLYHANGHOAEntities();
+
+        #region constructor
         public FrmMatHangKinhDoanh()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region LoadForm
+        private void LoadDgv()
+        {
+            dgvMatHangMain.DataSource = (from mh in db.MATHANGs.ToList()
+                                         from nsx in db.NHASANXUATs.ToList()
+                                         where mh.NHASANXUATID == nsx.NHASANXUATID
+                                         select new
+                                         {
+                                             MaMH = mh.MATHANGID,
+                                             TenMH = mh.TENMH,
+                                             ThanhPhan = mh.THANHPHAN,
+                                             DonViTinh = mh.DONVITINH,
+                                             NSX = nsx.TEN,
+                                             NhaSanXuatID = nsx.NHASANXUATID
+                                         })
+                                    .ToList();
+        }
+
+        private void InitControl()
+        {
+            cbxNhaSanXuat.DataSource = db.NHASANXUATs.ToList();
+            cbxNhaSanXuat.DisplayMember = "TEN";
+            cbxNhaSanXuat.ValueMember = "NHASANXUATID";
+        }
+        private void FrmMatHangKinhDoanh_Load(object sender, EventArgs e)
+        {
+            InitControl();
+            LoadDgv();
+            
+        }
+        #endregion
+
+        #region sự kiện ngầm
+        private void dgvMatHang_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            try
+            {
+                int mhID = (int) dgvMatHang.GetFocusedRowCellValue("MaMH");
+                txtTenMH.Text = (string)dgvMatHang.GetFocusedRowCellValue("TenMH");
+                txtDonViTinh.Text = (string)dgvMatHang.GetFocusedRowCellValue("DonViTinh");
+                txtThanhPhan.Text = (string)dgvMatHang.GetFocusedRowCellValue("ThanhPhan");
+                cbxNhaSanXuat.SelectedValue = (int)dgvMatHang.GetFocusedRowCellValue("NhaSanXuatID");
+            }
+            catch
+            {
+            }
+        }
+        #endregion
+
+        #region sự kiện
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MATHANG mh = new MATHANG();
+                mh.TENMH = txtTenMH.Text;
+                mh.DONVITINH = txtDonViTinh.Text;
+                mh.THANHPHAN = txtThanhPhan.Text;
+                int sx = int.Parse(cbxNhaSanXuat.SelectedValue.ToString());
+                
+                mh.NHASANXUATID = sx;
+
+
+              //  mh.MATHANGID = 100;
+
+                db.MATHANGs.Add(mh);
+                db.SaveChanges();
+
+                MessageBox.Show("Thêm mặt hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDgv();
+
+            }
+            catch
+            {
+               
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = (int)dgvMatHang.GetFocusedRowCellValue("MaMH");
+                MATHANG mh = db.MATHANGs.Where(p => p.MATHANGID == id).First();
+                mh.TENMH = txtTenMH.Text;
+                mh.DONVITINH = txtDonViTinh.Text;
+                mh.THANHPHAN = txtThanhPhan.Text;
+                mh.NHASANXUATID = (int)cbxNhaSanXuat.SelectedValue;
+
+                db.SaveChanges();
+
+                MessageBox.Show("Sửa thông tin mặt hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDgv();
+            }
+            catch
+            {
+                MessageBox.Show("Chưa có mặt hàng nào được chọn", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = (int)dgvMatHang.GetFocusedRowCellValue("MaMH");
+                MATHANG mh = db.MATHANGs.Where(p => p.MATHANGID == id).First();
+
+                DialogResult rs = MessageBox.Show("Bạn có chắc chắn xóa mặt hàng " + mh.TENMH + "?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (rs == DialogResult.Cancel) return;
+
+                db.MATHANGs.Remove(mh);
+                db.SaveChanges();
+
+                MessageBox.Show("Sửa thông tin mặt hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDgv();
+            }
+            catch
+            {
+                MessageBox.Show("Chưa có mặt hàng nào được chọn", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+       
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+
+        private void btnLamTrang_Click(object sender, EventArgs e)
+        {
+            txtTenMH.Text = "";
+            txtDonViTinh.Text = "";
+            txtThanhPhan.Text = "";
+            cbxNhaSanXuat.SelectedIndex = 0;
+        }
+        #endregion
+
+        private void tbTimKiem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbxNhaSanXuat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
--- QLBanHang/QLBanHang/GUI/FrmNhaSanXuat.cs
+++ QLBanHang/QLBanHang/GUI/FrmNhaSanXuat.cs
@@ -8,13 +8,125 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QLBanHang.GUI
+namespace QuanLyHangHoa.GUI
 {
     public partial class FrmNhaSanXuat : Form
     {
+        private QUANLYHANGHOAEntities db = new QUANLYHANGHOAEntities();
+
+        #region constructor
         public FrmNhaSanXuat()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region LoadForm
+        private void LoadDgv()
+        {
+            dgvNSXMain.DataSource = (from nsx in db.NHASANXUATs
+                                     select new
+                                     {
+                                         MaNSX = nsx.NHASANXUATID,
+                                         TenNSX = nsx.TEN,
+                                         NamThanhLap = nsx.NAMTHANHLAP
+                                     }).ToList();
+        }
+        private void FrmNhaSanXuat_Load(object sender, EventArgs e)
+        {
+            LoadDgv();   
+        }
+        #endregion
+
+        #region sự kiện ngầm
+        private void dgvNSX_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            try
+            {
+                int namthanhlap = (int)dgvNSX.GetFocusedRowCellValue("NamThanhLap");
+                txtTenNSX.Text = (string) dgvNSX.GetFocusedRowCellValue("TenNSX");
+                txtNamThanhLap.Value = namthanhlap;
+            }
+            catch
+            {
+
+            }
+        }
+        #endregion
+
+        #region sự kiện
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NHASANXUAT nsx = new NHASANXUAT();
+                nsx.TEN = txtTenNSX.Text;
+                nsx.NAMTHANHLAP = (int) txtNamThanhLap.Value;
+
+                db.NHASANXUATs.Add(nsx);
+                db.SaveChanges();
+
+                MessageBox.Show("Thêm thông tin nhà sản xuất thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDgv();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnLamTrang_Click(object sender, EventArgs e)
+        {
+            txtTenNSX.Text = "";
+            txtNamThanhLap.Value = 1900;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = (int)dgvNSX.GetFocusedRowCellValue("MaNSX");
+                NHASANXUAT nsx = db.NHASANXUATs.Where(p => p.NHASANXUATID == id).First();
+                nsx.TEN = txtTenNSX.Text;
+                nsx.NAMTHANHLAP = (int)txtNamThanhLap.Value;
+
+                db.SaveChanges();
+
+                MessageBox.Show("Sửa thông tin nhà nhà sản xuất thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDgv();
+            }
+            catch
+            {
+                MessageBox.Show("Chưa có nhà sản xuất nào được chọn", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = (int)dgvNSX.GetFocusedRowCellValue("MaNSX");
+                NHASANXUAT nsx = db.NHASANXUATs.Where(p => p.NHASANXUATID == id).First();
+
+                DialogResult rs = MessageBox.Show("Bạn có chắc chẵn xóa thông tin của nhà sản xuất " + nsx.TEN + "?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (rs == DialogResult.Cancel) return;
+
+                db.NHASANXUATs.Remove(nsx);
+                db.SaveChanges();
+
+                MessageBox.Show("Xóa thông tin nhà nhà sản xuất thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadDgv();
+            }
+            catch
+            {
+                MessageBox.Show("Chưa có nhà sản xuất nào được chọn", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
     }
 }
